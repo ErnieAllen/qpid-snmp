d1e754297b84b6acebdb777987a622988e549b1f
Ernie Allen <eallen@redhat.com>
2012-05-15 16:44:08 -0400

    Added makefiles for the traps directory

---
+++ agent/Release/src/traps/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/traps/rhm010Events.c 
+
+OBJS += \
+./src/traps/rhm010Events.o 
+
+C_DEPS += \
+./src/traps/rhm010Events.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/traps/%.o: ../src/traps/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
